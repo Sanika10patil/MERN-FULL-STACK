290cb590b07fa2cf0227faa8f37c97cd685af9ca
Sanika <sanikapatil550@gmail.com>
2024-12-28 22:58:15 +0530

    started project website

---
+++ reactjs/hariominnovations/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ContactUs from "./Pages/ContactUs";
+import './CSS/style.css'
+
+function App() {
+  return (
+    <div>
+      <ContactUs />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjs/hariominnovations/src/Pages/ContactUs.jsx
@@ -0,0 +1,12 @@
+import MainNavBar from "../Commponents/MainNavBar"
+
+const ContactUs = () => {
+    return (
+        <div> 
+            <MainNavBar/>
+        <h1>contact Us Page</h1>
+        </div>
+    );
+};
+
+export default ContactUs;
