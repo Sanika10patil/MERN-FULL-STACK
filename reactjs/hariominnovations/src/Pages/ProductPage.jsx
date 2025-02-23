08221b203cf16f368f3ab57d69fcceb22eac4510
Sanika <sanikapatil550@gmail.com>
2025-02-23 21:38:37 +0530

    node.js

--- reactjs/hariominnovations/src/Pages/ProductPage.jsx
+++ reactjs/hariominnovations/src/Pages/ProductPage.jsx
@@ -8,10 +8,12 @@ import Application from "./../images/New-Img.png"
 import arrow from "./../images/contactus-arrow.png"
 import rocket from "./../images/rocket.png"
 import lastimage from "./../images/product-page-footer.png"
-import readMore from "./../images/arrow-product-page.png"
+import readMore from "./../images/contactus-arrow.png"
 import frame from "./../images/Frame.png"
 import framelast from "./../images/Frame 39.png"
 import MainNavBar from '../Commponents/MainNavBar';
+import white from "./../images/white.png"
+import star from "./../images/star.png"
  
 const ProductPage = () => {
   return (
@@ -31,12 +33,14 @@ const ProductPage = () => {
       <div className="container">
         <div className="row">
           <div className='col-md-6 align-items-center justify-content-center'>
-            <h4>Billing Software in India for Small Businesses</h4>
+            <h4>Billing Software in India for
+              <br></br> Small Businesses</h4>
             <br />
-            <p>No one loves, seeks, or desires pain simply for the sake of pain. However,
-              there are times when effort and pain can lead to significant pleasure.</p>
+            <p>No one loves, seeks, or desires pain simply for the sake of pain.
+              <br></br> However,there are times when effort and pain can lead to 
+              <br></br>significant pleasure.</p>
             <div className="item">
-              <img src={group} alt="bullet" className="image" />
+              <img src={group} alt="bullet" className="image"/>
               <div className="description">
                 <p>Assess your position relative to your competitors.</p>
               </div>
@@ -55,10 +59,11 @@ const ProductPage = () => {
             </div>
            
             <Button className="button">VIEW ALL CASES</Button>
-            <img src={arrow} className="img-arrow"/>
+          <img src={arrow} className="img-arrow"/>
+            
           </div>
           <div className='col-md-6 d-flex align-items-center justify-content-center'>
-            <img src={Application} /> 
+            <img src={Application} width="200%"/> 
           </div>
         </div>
        
@@ -72,8 +77,9 @@ const ProductPage = () => {
       <div className="item-product d-flex">
         <img src={rocket} alt="bullet" className="image-product img-fluid" />
         <div className="description-product">
-          <h3>Manage cashflow seamlessly</h3>
-          <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digital services to help achieve their business success.</p>
+          <h3>Manage cashflow <br></br>seamlessly</h3>
+          <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digital services to help achieve 
+            their business success.</p>
         </div>
       </div>
     </div>
@@ -81,10 +87,11 @@ const ProductPage = () => {
     {/* Second Column */}
     <div className="col-md-4 d-flex align-items-center justify-content-center">
       <div className="item-product d-flex">
-        <img src={rocket} alt="bullet" className="image-product img-fluid" />
+        <img src={white} alt="bullet" className="image-product img-fluid" />
         <div className="description-product">
-          <h3>Online/Offline billing</h3>
-          <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digital services to help achieve their business success.</p>
+          <h3>Online/Offline <br></br>billing</h3>
+          <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digital services
+           to help achieve their business success.</p>
         </div>
       </div>
     </div>
@@ -92,9 +99,9 @@ const ProductPage = () => {
     {/* Third Column */}
     <div className="col-md-4 d-flex align-items-center justify-content-center">
       <div className="item-product d-flex">
-        <img src={rocket} alt="bullet" className="image-product img-fluid" />
+        <img src={white} alt="bullet" className="image-product img-fluid" />
         <div className="description-product">
-          <h3>Keep data safe with backups</h3>
+          <h3>Keep data safe with<br></br> backups</h3>
           <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digital services to help achieve their business success.</p>
         </div>
       </div>
@@ -107,10 +114,11 @@ const ProductPage = () => {
     {/* First Column */}
     <div className="col-md-6 d-flex align-items-center justify-content-center">
       <div className="item-product d-flex">
-        <img src={rocket} alt="bullet" className="image-product img-fluid" />
+        <img src={white} alt="bullet" className="image-product img-fluid" />
         <div className="description-product">
-        <h3>Keep data safe with backups</h3>
-        <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digital services to help achieve their business success.</p>
+        <h3>Keep data safe with <br></br>backups</h3>
+        <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digital 
+        services to help achieve their business success.</p>
         </div>
       </div>
     </div>
@@ -118,10 +126,11 @@ const ProductPage = () => {
     {/* Second Column */}
     <div className="col-md-6 d-flex align-items-center justify-content-center">
       <div className="item-product d-flex">
-        <img src={rocket} alt="bullet" className="image-product img-fluid" />
+        <img src={white} alt="bullet" className="image-product img-fluid" />
         <div className="description-product">
-        <h3>Keep data safe with backups</h3>
-        <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digital services to help achieve their business success.</p>
+        <h3>Keep data safe with <br></br>backups</h3>
+        <p>At Hariom, we will act as the client's trusted advisor specializing in areas of digitalservices 
+          to help achieve their business success.</p>
         </div>
       </div>
     </div>
@@ -132,7 +141,7 @@ const ProductPage = () => {
 <div className="container">
   <div className="row">
     <div className='col-md-6 d-flex align-items-center justify-content-center'>
-      <img src={lastimage} alt="product-page-image" className="img-fluid" style={{ width: 'auto', height: '400px' }} />
+    <img src={lastimage} width="500%" height="200%"/> 
     </div>
    
     <div className='col-md-6 align-items-center justify-content-center'>
@@ -146,11 +155,12 @@ const ProductPage = () => {
         </div>
       </div>
       <div className="item-product-last">
-        <img src={frame} alt="bullet" className="image" />
+        <img src={frame}  />
         <div className="description-product-last">
           <h3>Technical Development</h3>
-          <p>A wise person always adheres to these principles in such matters: those who are similarly at fault are those who fail.</p>
-          <p>Read More <span><img src={readMore} className="img-arrow" alt="arrow" /></span></p>
+          <p>A wise person always adheres to these principles in such matters: 
+           those who are similarly at fault are those who fail.</p>
+          <p>Read More <span> <img src={readMore}/> </span></p>
         </div>
       </div>
     </div>
@@ -166,30 +176,40 @@ const ProductPage = () => {
     </div>
    
     <div className='col-md-10 d-flex flex-column align-items-center justify-content-between'>
-      <div className="item-product-last-1 ">
+      <div className="testimonial-card">
      
-        <img src={framelast} alt="bullet" className="image" />
-        <div className="description-product-last-1">
+        <img src={framelast} alt="bullet" className="image"/> 
+        <p className="testimonial-text">It is a long established fact that a 
+          <br></br>reader will be distracted by the
+          <br></br> readable content of a page when 
+          <br></br>looking at its layout.</p>
+        <div className="Stars">
+        <img src={star} width="50%"/> 
+        <h4>Lora simth</h4></div>
+         </div>
+      
+      <div className="testimonial-card">
+        <img src={framelast} alt="bullet" className="image"/>
+        <p className="testimonal-text">It is a long established fact that a<br></br> reader will be distracted by the<br>
+        </br> readable content of a page when <br>
+        </br>looking at its layout.</p>
          
-          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-         
+        
+        <div className="Star">  
+       <img src={star} width="50%"/> 
+       <h4>Lora simth</h4>
         </div>
-      </div>
-      <div className="item-product-last-1">
-        <img src={framelast} alt="bullet" className="image" />
-        <div className="description-product-last-1">
-         
-          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-         
         </div>
-        <div className="item-product-last-1">
+        <div className="testimonial-card">
         <img src={framelast} alt="bullet" className="image" />
-        <div className="description-product-last-1">
-         
-          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-         
-        </div>
-      </div>
+        <p className="testimonal-text">It is a long established fact that a reader will be distracted by  <br></br> 
+        readable content of a page when <br>
+        </br>looking at its layout.</p>
+        <div className="Star">
+        <img src={star} width="50%"/> 
+        <h4>Lora simth</h4>
+         </div>
+  
       </div>
     </div>
     <div className='col-md-1 d-flex align-items-center justify-content-center'>
